4e55bf9bafc3be29fcc34d5c58689444ac9b33c5
jarbasal <jarbasai@mailfence.com>
2021-10-15 12:55:26 +0100

    ocp

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
 from os.path import join, dirname
-from ovos_utils.log import LOG
+
 from json_database import JsonStorageXDG
 from nuvem_de_som import SoundCloud
-from ovos_utils.parse import fuzzy_match
 from ovos_plugin_common_play.ocp import MediaType, \
     PlaybackType
+from ovos_utils.parse import fuzzy_match
 from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill, \
     ocp_search
+from ovos_utils.log import LOG
 
 
 class SoundCloudSkill(OVOSCommonPlaybackSkill):
@@ -50,6 +51,9 @@ class SoundCloudSkill(OVOSCommonPlaybackSkill):
                 score += artist_score * 0.5 + title_score * 0.5
             else:
                 score += title_score * 0.85 + artist_score * 0.15
+            # TODO score penalty based on track length,
+            #  longer -> less likely to be a song
+            score -= idx * 2  # - 2% as we go down the results list
         else:
             if artist_score >= 85:
                 score += artist_score * 0.85 + title_score * 0.15
@@ -60,7 +64,7 @@ class SoundCloudSkill(OVOSCommonPlaybackSkill):
             else:
                 score += title_score * 0.7 + artist_score * 0.3
 
-        #LOG.debug(f"type: {searchtype} score: {score} artist:
+        # LOG.debug(f"type: {searchtype} score: {score} artist:
         # {match['artist']} title: {match['title']}")
         score = min((100, score))
         score -= idx * 5  # - 5% as we go down the results list
@@ -80,7 +84,9 @@ class SoundCloudSkill(OVOSCommonPlaybackSkill):
                 results = []
                 if searchtype == "tracks":
                     for r in SoundCloud.search_tracks(phrase):
-                        r["uri"] = "soundcloud//" + r["url"]
+                        if r["duration"] <= 60:
+                            continue  # filter previews
+                        r["uri"] = "ydl//" + r["url"]
                         r["match_confidence"] = self.calc_score(
                             phrase, r, searchtype=searchtype, idx=len(results))
                         yield r
@@ -88,81 +94,85 @@ class SoundCloudSkill(OVOSCommonPlaybackSkill):
                 elif searchtype == "artists":
                     n = 0
                     for a in SoundCloud.search_people(phrase):
-                        for r in a["tracks"]:
-                            # - 15% score for each artist
-                            conf = self.calc_score(phrase, r,
-                                                   searchtype=searchtype,
-                                                   idx=len(results)) - n * 15
-                            r["uri"] = "soundcloud//" + r["url"]
-                            r["match_confidence"] = conf
-                            yield r
-                            results.append(r)
-                        n += 1
+                        pl = []
+                        for idx, v in enumerate(a["tracks"]):
+                            if v["duration"] <= 60:
+                                continue  # filter previews
+                            pl.append({
+                                "match_confidence": self.calc_score(phrase, v,
+                                                                    searchtype="artists",
+                                                                    idx=idx),
+                                "media_type": MediaType.MUSIC,
+                                "length": v["duration"] * 1000,
+                                "uri": "ydl//" + v["url"],
+                                "playback": PlaybackType.AUDIO,
+                                "image": v["image"],
+                                "bg_image": v["image"],
+                                "skill_icon": self.skill_icon,
+                                "title": v["title"],
+                                "artist": v["artist"],
+                                "skill_id": self.skill_id
+                            })
+                        if not pl:
+                            continue
+                        entry = dict(pl[0])
+                        entry.pop("uri")
+
+                        entry["title"] = entry["artist"] + " (Featured Tracks)"
+                        entry["playlist"] = pl
+                        # bonus for artists with more tracks
+                        entry["match_confidence"] += len(a["tracks"])
+                        yield entry
+                        results.append(entry)
+
+                    n += 1
 
                 elif searchtype == "sets":
                     n = 0
                     for s in SoundCloud.search_sets(phrase):
-                        for r in s["tracks"]:
-                            # - 15% score for each set
-                            conf = self.calc_score(phrase, r,
-                                                   searchtype=searchtype,
-                                                   idx=len(results)) - n * 15
-                            r["uri"] = "soundcloud//" + r["url"]
-                            r["match_confidence"] = conf
-                            yield r
-                            results.append(r)
-                        n += 1
+                        pl = []
+                        for idx, v in enumerate(s["tracks"]):
+                            if v["duration"] <= 60:
+                                continue  # filter previews
+
+                            pl.append({
+                                "match_confidence": self.calc_score(
+                                    phrase, v, searchtype="sets", idx=idx),
+                                "media_type": MediaType.MUSIC,
+                                "length": v["duration"] * 1000,
+                                "uri": "ydl//" + v["url"],
+                                "playback": PlaybackType.AUDIO,
+                                "image": v["image"],
+                                "bg_image": v["image"],
+                                "skill_icon": self.skill_icon,
+                                "title": v["title"],
+                                "artist": v["artist"],
+                                "skill_id": self.skill_id
+                            })
+                        if not pl:
+                            continue
+                        entry = dict(pl[0])
+                        entry["playlist"] = pl
+                        entry.pop("uri")
+                        entry["title"] = s["title"] + " (Playlist)"
+                        yield entry
+                        results.append(entry)
+
+                    n += 1
                 else:
                     for r in SoundCloud.search(phrase):
-                        r["uri"] = "soundcloud//" + r["url"]
+                        if r["duration"] < 60:
+                            continue  # filter previews
+                        r["uri"] = "ydl//" + r["url"]
                         r["match_confidence"] = self.calc_score(
                             phrase, r, searchtype=searchtype, idx=len(results))
                         yield r
                         results.append(r)
             except Exception as e:
-                # soundcloud can break at any time...
-                self.log.error("soundcloud search failed!")
-                self.log.exception(e)
                 return []
             if self.settings["cache"]:
                 self._search_cache[searchtype][phrase] = results
                 self._search_cache.store()
-
-    @ocp_search()
-    def search_tracks(self, phrase, media_type=MediaType.GENERIC):
-        # match the request media_type
-        base_score = 0
-        if media_type == MediaType.MUSIC:
-            base_score += 15
-
-        if self.voc_match(phrase, "soundcloud"):
-            # explicitly requested soundcloud
-            base_score += 30
-            phrase = self.remove_voc(phrase, "soundcloud")
-
-        #LOG.debug("searching soundcloud tracks")
-        for r in self.search_soundcloud(phrase, searchtype="tracks"):
-            score = r["match_confidence"]
-            if score < 35:
-                continue
-            # crude attempt at filtering non music / preview tracks
-            # we might still get podcasts, would be nice to handle that in separate
-            if r["duration"] < 60:
-                continue
-
-            yield {
-                "match_confidence": score + base_score,
-                "media_type": MediaType.MUSIC,
-                "length": r["duration"] * 1000,  # seconds to milliseconds
-                "uri": r["uri"],
-                "playback": PlaybackType.AUDIO,
-                "image": r["image"],
-                "bg_image": r["image"],
-                "skill_icon": self.skill_icon,
-                "title": r["title"],
-                "artist": r["artist"],
-                "skill_id": self.skill_id
-            }
 
     @ocp_search()
     def search_artists(self, phrase, media_type=MediaType.GENERIC):
@@ -175,30 +185,10 @@ class SoundCloudSkill(OVOSCommonPlaybackSkill):
             # explicitly requested soundcloud
             base_score += 50
             phrase = self.remove_voc(phrase, "soundcloud")
-        #LOG.debug("searching soundcloud artists")
-        for r in self.search_soundcloud(phrase, searchtype="artists"):
-            score = r["match_confidence"]
-            if score < 35:
-                continue
-            # crude attempt at filtering non music / preview tracks
-            # we might still get podcasts, would be nice to handle that in separate
-            if r["duration"] < 60:
-                continue
 
-            yield {
-                "match_confidence": score + base_score,
-                # TODO this can actuall go above 100
-                "media_type": MediaType.MUSIC,
-                "length": r["duration"] * 1000,  # seconds to milliseconds
-                "uri": r["uri"],
-                "playback": PlaybackType.AUDIO,
-                "image": r["image"],
-                "bg_image": r["image"],
-                "skill_icon": self.skill_icon,
-                "title": r["title"],
-                "artist": r["artist"],
-                "skill_id": self.skill_id
-            }
+        LOG.debug("searching soundcloud artists")
+        for pl in self.search_soundcloud(phrase, "artists"):
+            yield pl
 
     @ocp_search()
     def search_sets(self, phrase, media_type=MediaType.GENERIC):
@@ -211,19 +201,37 @@ class SoundCloudSkill(OVOSCommonPlaybackSkill):
             # explicitly requested soundcloud
             base_score += 30
             phrase = self.remove_voc(phrase, "soundcloud")
-        #LOG.debug("searching soundcloud sets")
-        for r in self.search_soundcloud(phrase, searchtype="sets"):
+
+        LOG.debug("searching soundcloud sets")
+        for pl in self.search_soundcloud(phrase, "sets"):
+            yield pl
+
+    @ocp_search()
+    def search_tracks(self, phrase, media_type=MediaType.GENERIC):
+        # match the request media_type
+        base_score = 0
+        if media_type == MediaType.MUSIC:
+            base_score += 10
+
+        if self.voc_match(phrase, "soundcloud"):
+            # explicitly requested soundcloud
+            base_score += 30
+            phrase = self.remove_voc(phrase, "soundcloud")
+
+        LOG.debug("searching soundcloud tracks")
+        for r in self.search_soundcloud(phrase, searchtype="tracks"):
             score = r["match_confidence"]
             if score < 35:
                 continue
             # crude attempt at filtering non music / preview tracks
-            # we might still get podcasts, would be nice to handle that in separate
             if r["duration"] < 60:
+                continue
+            # we might still get podcasts, would be nice to handle that better
+            if r["duration"] > 60 * 45: # >45 min is probably not music :shrug:
                 continue
 
             yield {
-                "match_confidence": score + base_score ,
-                # TODO this can actuall go above 100
+                "match_confidence": score + base_score,
                 "media_type": MediaType.MUSIC,
                 "length": r["duration"] * 1000,  # seconds to milliseconds
                 "uri": r["uri"],
